f29848b6edee5617002c5fff73f154bb25aec9d1
wangxinjie <wangxinjie@ky-tech.com.cn>
2019-09-04 11:08:10 +0800

    first commit

---
+++ src/main/java/com/mmgg/DispaterServlet.java
@@ -0,0 +1,28 @@
+package com.mmgg;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 161196
+ */
+public class DispaterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("===========================");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
